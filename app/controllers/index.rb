1b3a6ae0895e3b2a06b9e267aa8488c000935f30
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-20 18:10:42 -0700

    users are able to save recipes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  erb :index
+ redirect '/recipes' 
 end
 
 post '/recipes' do
@@ -7,12 +7,24 @@ post '/recipes' do
   @result = Yummly.search(search_string, :maxResult => 20)
   @result_total = @result.matches.size
 
+  @saved_recipes = Recipe.all
   erb :index
 end
 
 get '/recipes/:result_id' do
-  params[:result_id] 
   @recipe = Yummly.find(params[:result_id])
 
   erb :recipe
+end
+
+post '/recipes/:result_id' do
+  recipe = Yummly.find(params[:result_id])
+
+  saved_recipe = Recipe.new(name: recipe.name, yummly_id: recipe.id, ingredients: recipe.ingredients)
+
+  if saved_recipe.save
+    redirect '/'
+  else
+    status 400
+  end
 end
--- db/migrate/20141020173657_create_recipes_table.rb
+++ db/migrate/20141020173657_create_recipes_table.rb
@@ -1,6 +1,7 @@
 class CreateRecipesTable < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
+      t.string :name, :yummly_id
       t.text :ingredients
       t.timestamps 
     end
