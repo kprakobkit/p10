efdbe8a8e7d70befc397e42b0d6f7d76fd6f0a57
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-20 18:43:08 -0700

    add draft test specs for index controller

---
+++ spec/controller/index_controller_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../spec_helper'
+
+describe :Controller do
+  context "get /recipes" do
+    it "should display all saved recipes" do
+    end
+    it "should display search results from Yummly's API" do
+    end
+    it "should display all saved recipes" do
+    end
+  end
+
+  context "get/recipes/:id" do
+    it "should display the recipe with the specified id from Yummly's API" do
+    end
+  end
+
+  context "post 'recipes/recipe_id' " do
+  end
+end
