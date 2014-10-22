879b64ac5760ab2fcd7628f31165c6bbd4d605d5
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-21 18:55:08 -0700

    refactor position of js files and structure of application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,31 @@
 $(document).ready(function() {
-  $('.scheduled_recipes').on('click', '.delete', function(event) {
-    event.preventDefault();
-    deleteUrl = "/recipes/delete/" + $(this).parent().attr('id');
-    Server.deleteRequest(deleteUrl);
-  });
+  Controller.initialize();
 });
+
+var Controller = (function(){
+  function bindDeleteButton(){ 
+    $('.scheduled_recipes').on('click', '.delete', function(event) {
+      event.preventDefault();
+      deleteUrl = "/recipes/delete/" + $(this).parent().attr('id');
+      Server.deleteRequest(deleteUrl);
+    });
+  };
+
+  function initialize() {
+    bindDeleteButton();
+  };
+
+  return {
+    initialize: initialize
+  };
+})();
+
 
 var Server = (function(){
   function deleteRequest(deleteUrl) {
     var ajaxRequest = $.ajax({
       url: deleteUrl,
-      type: "POST" 
+        type: "POST" 
     }).done(View.removeListElement);
   };
 
